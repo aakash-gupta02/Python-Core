52f2c015b8cbca8cd86d5cc0bf1b6d240e7ae69d
Aakash <editsky597@gmail.com>
2025-06-02 11:33:05 +0530

    python core day1

---
+++ cwh python/setup/comments.py
@@ -0,0 +1,4 @@
+a = "helllo ,\n how are you\t are u doing\\ \" fine"
+
+print(a)
+
---
+++ cwh python/setup/main.py
@@ -0,0 +1,36 @@
+print("hello world")
+
+print("Helllo bhai")
+
+_hello = "Harry"
+
+print(_hello)
+
+# type casting
+
+a = 123
+
+print(a)
+print(type(a))
+
+b = str(a)
+print(b, type(b))
+
+c = "0"
+print(c, type(c))
+
+d = int(c)
+print(d, type(d))
+
+e = bool(d)
+print(e, type(e))
+
+
+
+_a = int(input("Enter a number"))
+_b = int(input("Enter 2nd number"))
+
+
+print(_a + _b)
+
+
