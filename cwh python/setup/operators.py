9112fdd38b62bc01917b26b1ee6e9b61a8f2ae1c
Aakash <editsky597@gmail.com>
2025-06-02 17:34:26 +0530

    operators

---
+++ cwh python/setup/operators.py
@@ -0,0 +1,36 @@
+a = 34
+b=69
+
+
+#arithematic operators
+
+
+print("a + b  = ", a + b)    # Addition
+print("a - b  = ", a - b)    # Subtraction
+print("a * b  = ", a * b)    # Multiplication
+print("a / b  = ", a / b)    # Division (float result)
+print("a // b = ", a // b)   # Floor Division (integer result)
+print("a % b  = ", a % b)    # Modulus (remainder)
+print("a ** b = ", a ** b)   # Exponentiation (power)
+
+
+
+
+a = True
+b = False
+
+print("a and b = ", a and b)  # False
+print("a or b  = ", a or b)   # True
+print("not a   = ", not a)    # False
+
+
+
+x = 10
+y = 5
+
+print("x == y  = ", x == y)   # False
+print("x != y  = ", x != y)   # True
+print("x > y   = ", x > y)    # True
+print("x < y   = ", x < y)    # False
+print("x >= y  = ", x >= y)   # True
+print("x <= y  = ", x <= y)   # False
